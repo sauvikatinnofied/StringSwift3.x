c8bcfe9b0373ed49f813d55ed65c362378c1c111
Sauvik Dolui <sauvikdolui@Sauviks-MacBook-Air.local>
2017-03-03 02:06:27 +0530

    Added few more string helper methods

--- TestPlayground.playground/Contents.swift
+++ TestPlayground.playground/Contents.swift
@@ -1,53 +1,7 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-
-// MARK:- STRINGS EXTENSION
-extension String {
-    
-    // MARK: - COMPUTED PROPETIES
-    var length: Int {
-        return self.characters.count
-    }
-    var reversed: String {
-        return String(self.characters.reversed())
-    }
-    var isPalindome: Bool {
-        return self == reversed
-    }
-    var trimmed: String {
-        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-    }
-    
-    // MARK: SUBSTING GENERATION
-    func subString(start: Int, length: Int) -> String? {
-        if start + length <= self.length {
-            let subStringEndIndex = self.index(startIndex, offsetBy: length)
-            return self.substring(with: startIndex..<subStringEndIndex)
-        } else {
-            return nil
-        }
-    }
-    
-    func subStringUptoString(string: String) -> String? {
-        guard let index = self.range(of: string, options: .caseInsensitive) else {
-            return nil
-        }
-        return self.substring(to: index.lowerBound)
-    }
-    
-    // MARK: - CUSTOM OPERATORS
-    static func * (lsh: String, times: Int) -> String {
-        return Array(repeatElement(lsh, count: times)).joined(separator: "")
-    }
-    
-    // MARK: - LOCALIZATION
-    func localizedWithComment(comment: String?) -> NSString {
-        return NSLocalizedString(self, comment: comment ?? "") as NSString
-    }
-    
-}
 
 // 5.3 Replaceing occurance of a string with another one
 let string = "We are here to Die, only a few have a valuable death"
@@ -60,7 +14,7 @@ let revered = string.reversed
 // htaed elbaulav a evah wef a ylno ,eiD ot ereh era eW
 
 // 5.5 Palindrome Check
-print("HelloolleH".isPalindome)
+print("HelloolleH".isPalindrome)
 // true
 
 // 5.6 Words
@@ -84,3 +38,5 @@ print(singleLine * N)
 
 let subString = string.subStringUptoString(string: "die")
 let indexSubString = string.subString(start: 0, length: 4)
+let hasSubString = "Sauvik Dolui".hasSubString(string: "Sauvik")// case insensitive search
+let hasSubStringComparingCase = "Sauvik Dolui".hasSubString(string: "sauvik", compareOption: .literal) // case sensetive search
